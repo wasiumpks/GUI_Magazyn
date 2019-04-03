29418d3e1d6eaf0c070e6f218245f4512132998a
Maciej Wasiak <maciek.wasiak44@gmail.com>
2019-04-03 22:19:53 +0200

    update

--- src/pl/maciejwasiak/magazyn/TooManyItemsException.java
+++ src/pl/maciejwasiak/magazyn/TooManyItemsException.java
@@ -1,8 +1,15 @@
+/*
+ * Copyright (c) 2019.
+ * Created by Maciej Wasiak
+ * s18952@pjwstk.edu.pl
+ */
+
 package pl.maciejwasiak.magazyn;
 
 public class TooManyItemsException extends Exception {
 
     public TooManyItemsException(String message) {
         super(message);
+
     }
 }
